3c4be012dc513e1384c40cc8ec859fd8d081117a
raudelf <rawdell@outlook.com>
2020-03-14 17:15:25 -0500

    Got the comments post working

--- hubs/post-router.js
+++ hubs/post-router.js
@@ -21,25 +21,27 @@ router.post('/', (req, res) => {
 });
 
 router.post('/:id/comments',  (req, res) => {
-    const id = req.params.id;
     const commentObj = req.body;
+    console.log('Obj: ', commentObj);
 
-    Hubs.insertComment(commentObj, id)
-        .then((hub, id) => {
-            if(id) {
+    Hubs.insertComment(commentObj)
+        .then((hub) => {
+            if(hub.post_id) {
                 console.log('New Comment: ', hub);
                 res.status(201).json({hub});
-            };
-        })
-        .catch((err, id) => {
-            if(!id) {
-                console.log('The post with the specified ID does not exist.');
-                res.status(404).json({ message: 'The post with the specified ID does not exist.' }) 
             } else {
-                console.log('There was an error while saving the comment to the database.');
-                res.status(500).json({ error: 'There was an error while saving the comment to the database.' })
+                console.log('The post with the specified ID does not exist.')
             }
         })
+        .catch((err) => {
+            if(!err.text) {
+                console.log('Please provide text for the comment.');
+                res.status(404).json({ errorMessage: 'Please provide text for the comment.' }); 
+            } else {
+                console.log('There was an error while saving the comment to the database.');
+                res.status(500).json({ error: 'There was an error while saving the comment to the database.' });
+            };
+        });
 })
 
 module.exports = router;
