f8f301b207c2376bb95e0cfa8bac09e713b254b3
raudelf <rawdell@outlook.com>
2020-03-14 15:08:06 -0500

    finished creating the first POST request

--- hubs/post-router.js
+++ hubs/post-router.js
@@ -2,3 +2,22 @@ const express = require('express');
 const Hubs = require('./hubs-model');
 
 const router = express.Router();
+
+router.post('/', (req, res) => {
+    Hubs.insert(req.body)
+        .then(hub => {
+            console.log('New Post: ', hub);
+                res.status(201).json(hub);
+        })
+        .catch(err => {
+            if(!err.title && !err.contents) {
+                console.log('Please provide title and contents for the post.');
+                res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' });
+            } else {
+                console.log('There was an error while saving the post to the database: ', err)
+                res.status(500).json({ error: 'There was an error while saving the post to the database' });
+            }
+        })
+})
+
+module.exports = router;
