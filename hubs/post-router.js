e0bec7e8bec2b2357dab82afb4f763ee3530d0cc
raudelf <rawdell@outlook.com>
2020-03-14 15:49:12 -0500

    started the comment post endpoint but can't get it to work properly just yet..

--- hubs/post-router.js
+++ hubs/post-router.js
@@ -14,8 +14,30 @@ router.post('/', (req, res) => {
                 console.log('Please provide title and contents for the post.');
                 res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' });
             } else {
-                console.log('There was an error while saving the post to the database: ', err)
+                console.log('There was an error while saving the post to the database: ', err);
                 res.status(500).json({ error: 'There was an error while saving the post to the database' });
+            };
+        });
+});
+
+router.post('/:id/comments',  (req, res) => {
+    const id = req.params.id;
+    const commentObj = req.body;
+
+    Hubs.insertComment(commentObj, id)
+        .then((hub, id) => {
+            if(id) {
+                console.log('New Comment: ', hub);
+                res.status(201).json({hub});
+            };
+        })
+        .catch((err, id) => {
+            if(!id) {
+                console.log('The post with the specified ID does not exist.');
+                res.status(404).json({ message: 'The post with the specified ID does not exist.' }) 
+            } else {
+                console.log('There was an error while saving the comment to the database.');
+                res.status(500).json({ error: 'There was an error while saving the comment to the database.' })
             }
         })
 })
