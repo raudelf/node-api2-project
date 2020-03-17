56688759e0772aabd9ca3b323c5062f72ca569ca
raudelf <rawdell@outlook.com>
2020-03-17 00:16:20 -0500

    Finished the delete endpoint

--- hubs/manipulation-router.js
+++ hubs/manipulation-router.js
@@ -3,4 +3,28 @@ const Hubs = require('./hubs-model');
 
 const router = express.Router();
 
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+
+    Hubs.findById(id)
+        .then(obj => {
+            res.status(201).json({message: 'Deleted the following object:', obj});
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'There was an issue retrieving object for the Delete endpoint', err });
+        });
+
+    Hubs.remove(id)
+        .then(deleted => {
+            if (deleted) {
+                res.status(200).json(deleted).end();
+            } else {
+                res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+            };
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'The post could not be removed.', err})
+        })
+});
+
 module.exports = router;
