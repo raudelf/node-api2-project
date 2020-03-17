a815dd3a5fcd3a8aadb3587df41bf7627a14dddf
raudelf <rawdell@outlook.com>
2020-03-16 23:55:51 -0500

    imported the manipulation router

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const postRouter = require('./hubs/post-router');
 const getRouter = require('./hubs/get-router');
+const manipulationRouter = require('./hubs/manipulation-router');
 
 const server = express();
 
 server.use(express.json());
 
 // Routers
-server.use('/api/posts', postRouter)
-server.use('/api/posts', getRouter)
+server.use('/api/posts', postRouter);
+server.use('/api/posts', getRouter);
+server.use('/api/posts', manipulationRouter);
 
 // Landing Page Endpoint
 server.get('/', (req, res) => {
